07be4790b26d572910b49549b500cfc19435b916
RiB7 <rishabhbaranwal420@gmail.com>
2025-05-17 22:06:07 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import doctorRoutes from './routes/doctors'; // adjust path
+import doctorRoutes from './routes/doctors'; 
 
 const app = express();
 
